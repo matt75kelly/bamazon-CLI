cd7fe0ca0d5a2b1dd8786f8ea558e9dbeaed51df
Matthew Kelly <matthew75kelly@yahoo.com>
2018-08-13 18:30:31 -0500

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -15,17 +15,17 @@ CREATE TABLE products(
 );
 
 INSERT INTO products (product_name, department, selling_price, stock_quantity, product_sales)
-VALUES ("Crockpot", "Kitchen", 75.00, 5, 0),
-("Extreme Juicer", "Kitchen", 300.00, 3, 0),
-("5.1 Surround Sound System", "Electronics", 270.05, 10, 0),
-("65 inch 4k Smart TV", "Electronics", 3500.00, 2, 0),
-("Cordless 80 VDC Lawn Mower", "Outdoor", 489.80, 5, 0),
-("2 Burner Liquid Propane Grill", "Outdoor", 139.99, 10, 0),
-("wicker Patio Furniture Set", "Outdoor", 155.98, 8, 0),
-("20V Cordless Drill", "Home Improvement", 152.77, 12, 0),
-("Corded Shop Vacuum", "Home Improvement", 78.00, 14, 0),
-("Floral Semi-Sheer Curtain", "Home Decor", 34.85, 20, 0),
-("Beveled Oval Wall Mirror", "Home Decor", 58.44, 20, 0);
+VALUES ("Crockpot", "Kitchen", 75.00, 5, 0.00),
+("Extreme Juicer", "Kitchen", 300.00, 3, 0.00),
+("5.1 Surround Sound System", "Electronics", 270.05, 10, 0.00),
+("65 inch 4k Smart TV", "Electronics", 3500.00, 2, 0.00),
+("Cordless 80 VDC Lawn Mower", "Outdoor", 489.80, 5, 0.00),
+("2 Burner Liquid Propane Grill", "Outdoor", 139.99, 10, 0.00),
+("wicker Patio Furniture Set", "Outdoor", 155.98, 8, 0.00),
+("20V Cordless Drill", "Home Improvement", 152.77, 12, 0.00),
+("Corded Shop Vacuum", "Home Improvement", 78.00, 14, 0.00),
+("Floral Semi-Sheer Curtain", "Home Decor", 34.85, 20, 0.00),
+("Beveled Oval Wall Mirror", "Home Decor", 58.44, 20, 0.00);
 
 CREATE TABLE departments(
     department_id INTEGER(10) AUTO_INCREMENT NOT NULL,
